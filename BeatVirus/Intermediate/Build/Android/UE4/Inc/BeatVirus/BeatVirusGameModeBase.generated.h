2fdb9218744d7e82b939bea659f98486de737a2c
Pearson Lawrence <pearson.lawrence.official@gmail.com>
2018-09-14 15:38:34 -0700

    Fixed android support for Pearson computer, created base player movement based on touch start and movement

---
+++ BeatVirus/Intermediate/Build/Android/UE4/Inc/BeatVirus/BeatVirusGameModeBase.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BEATVIRUS_BeatVirusGameModeBase_generated_h
+#error "BeatVirusGameModeBase.generated.h already included, missing '#pragma once' in BeatVirusGameModeBase.h"
+#endif
+#define BEATVIRUS_BeatVirusGameModeBase_generated_h
+
+#define BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_RPC_WRAPPERS
+#define BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesABeatVirusGameModeBase(); \
+	friend struct Z_Construct_UClass_ABeatVirusGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ABeatVirusGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/BeatVirus"), NO_API) \
+	DECLARE_SERIALIZER(ABeatVirusGameModeBase)
+
+
+#define BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesABeatVirusGameModeBase(); \
+	friend struct Z_Construct_UClass_ABeatVirusGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ABeatVirusGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/BeatVirus"), NO_API) \
+	DECLARE_SERIALIZER(ABeatVirusGameModeBase)
+
+
+#define BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABeatVirusGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABeatVirusGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABeatVirusGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABeatVirusGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABeatVirusGameModeBase(ABeatVirusGameModeBase&&); \
+	NO_API ABeatVirusGameModeBase(const ABeatVirusGameModeBase&); \
+public:
+
+
+#define BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABeatVirusGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABeatVirusGameModeBase(ABeatVirusGameModeBase&&); \
+	NO_API ABeatVirusGameModeBase(const ABeatVirusGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABeatVirusGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABeatVirusGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABeatVirusGameModeBase)
+
+
+#define BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_12_PROLOG
+#define BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_RPC_WRAPPERS \
+	BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_INCLASS \
+	BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID BeatVirus_Source_BeatVirus_BeatVirusGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- BeatVirus/Intermediate/Build/Win64/BeatVirusEditor/Development/Engine/SharedPCH.Engine.cpp
+++ BeatVirus/Intermediate/Build/Win64/BeatVirusEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "E:/Projects/UE4/BeatVirus/BeatVirus/Intermediate/Build/Win64/BeatVirusEditor/Development/Engine/SharedPCH.Engine.h"
+#include "D:/ProjectsUE4/BeatVirus/BeatVirus/Intermediate/Build/Win64/BeatVirusEditor/Development/Engine/SharedPCH.Engine.h"

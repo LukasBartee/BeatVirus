fb838c34f9bad32d867f53d6299035e03b586260
Lukas Bartee <lukasbartee@comcast.net>
2018-09-13 17:27:55 -0700

    I think I fixed it

---
+++ BeatVirus/Intermediate/Build/Win64/BeatVirusEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "E:/Projects/UE4/BeatVirus/BeatVirus/Intermediate/Build/Win64/BeatVirusEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ BeatVirus/Source/BeatVirus/BeatVirusGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BeatVirusGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEATVIRUS_API ABeatVirusGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
